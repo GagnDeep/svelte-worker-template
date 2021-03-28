0058c650b8e0e6e0c4e3a93fe48e3bf5c02ebad6
GagnDeep <dp.gagn@gmail.com>
2021-03-29 00:12:55 +0530

    initial commit

---
+++ src/GlobalCSS.svelte
@@ -0,0 +1,26 @@
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  *{
+    box-sizing: border-box;
+  }
+
+    *::-webkit-scrollbar-track {
+    border-radius: 6px;
+    background-color: #f5f5f5;
+  }
+
+  *::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+    background-color: #f5f5f5;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: rgb(177, 177, 177);
+  }
+
+</style>
